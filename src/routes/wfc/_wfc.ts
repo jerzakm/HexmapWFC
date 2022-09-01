477ed00dee761958ae6787422332e29d837bd2af
jerzakm <jerzakm@gmail.com>
2022-09-01 14:20:11 +0200

    basic demo working

--- src/routes/wfc/_wfc.ts
+++ src/routes/wfc/_wfc.ts
@@ -4,19 +4,23 @@ export const initCanvasWfc = async (canvas: HTMLCanvasElement) => {
 	canvas.width = width;
 	canvas.height = height;
 
-	const gridSize = { x: 5, y: 5 };
+	const gridSize = { x: 10, y: 10 };
+	const cellSize = 30;
 
 	const grid: WfcSampleCell[][] = [];
+	const cells: WfcSampleCell[] = [];
 
 	for (let x = 0; x < gridSize.x; x++) {
 		grid[x] = [];
 		for (let y = 0; y < gridSize.y; y++) {
-			grid[x][y] = {
+			const cell = {
 				collapsed: false,
-				options: [...tileSet]
+				options: [...tileSet],
+				x,
+				y
 			};
-
-			console.log(grid[x][y]);
+			grid[x][y] = cell;
+			cells.push(cell);
 		}
 	}
 
@@ -25,10 +29,102 @@ export const initCanvasWfc = async (canvas: HTMLCanvasElement) => {
 	ctx.fillStyle = '#aaaaaa';
 	ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-	const tileImages: HTMLImageElement[] = [];
 	for (const tile of tileSet) {
-		const image = await loadImage(tile.imgSrc);
-		tileImages.push(image);
+		tile.image = await loadImage(tile.imgSrc);
+	}
+
+	// 1. Pick random position on grid and a random tile. Collapse it.
+	collapseTile(Math.floor(Math.random() * gridSize.x), Math.floor(Math.random() * gridSize.y));
+
+	for (let z = 0; z < 1000; z++) {
+		cells.sort((a, b) => {
+			return a.options.length - b.options.length;
+		});
+
+		for (let i = 0; i < cells.length; i++) {
+			if (!cells[i].collapsed) {
+				collapseTile(cells[i].x, cells[i].y);
+				break;
+			}
+		}
+	}
+
+	function collapseTile(x: number, y: number) {
+		//
+		const cell = grid[x][y];
+		cell.collapsed = true;
+		cell.tile = cell.options[Math.floor(Math.random() * cell.options.length)];
+
+		if (cell.tile.image) {
+			ctx?.drawImage(cell.tile.image, x * cellSize, y * cellSize);
+		}
+
+		// 1.1 After collapsing a tile, recalculate neighbour options
+		const neighbours: (WfcSampleCell | null)[] = Object.values(getNeighbours(x, y));
+		for (const cell of neighbours) {
+			if (cell) calculateOptions(cell);
+		}
+	}
+
+	function calculateOptions(cell: WfcSampleCell) {
+		const n = getNeighbours(cell.x, cell.y);
+
+		let options = [...tileSet];
+
+		if (n.up?.collapsed) {
+			const connector = n.up.tile?.down;
+			options = options.filter((p) => {
+				return p.up === connector;
+			});
+		}
+		if (n.down?.collapsed) {
+			const connector = n.down.tile?.up;
+			options = options.filter((p) => {
+				return p.down === connector;
+			});
+		}
+		if (n.left?.collapsed) {
+			const connector = n.left.tile?.right;
+			options = options.filter((p) => {
+				return p.left === connector;
+			});
+		}
+		if (n.right?.collapsed) {
+			const connector = n.right.tile?.left;
+			options = options.filter((p) => {
+				return p.right === connector;
+			});
+		}
+
+		cell.options = options;
+	}
+
+	function getNeighbours(x: number, y: number) {
+		const neighbours: WfcSampleCellNeighbours = {
+			up: null,
+			down: null,
+			left: null,
+			right: null
+		};
+
+		// above
+		if (y - 1 >= 0) {
+			neighbours.up = grid[x][y - 1];
+		}
+		//below
+		if (y + 1 < gridSize.y) {
+			neighbours.down = grid[x][y + 1];
+		}
+		// left
+		if (x - 1 >= 0) {
+			neighbours.left = grid[x - 1][y];
+		}
+		// right
+		if (x + 1 < gridSize.x) {
+			neighbours.right = grid[x + 1][y];
+		}
+
+		return neighbours;
 	}
 };
 
@@ -89,6 +185,7 @@ const tileSet: WfcSampleTile[] = [
 
 interface WfcSampleTile {
 	imgSrc: string;
+	image?: HTMLImageElement;
 	// white - false, black - true
 	up: boolean;
 	down: boolean;
@@ -99,5 +196,14 @@ interface WfcSampleTile {
 interface WfcSampleCell {
 	collapsed: boolean;
 	tile?: WfcSampleTile;
-	options?: WfcSampleTile[];
+	options: WfcSampleTile[];
+	x: number;
+	y: number;
+}
+
+interface WfcSampleCellNeighbours {
+	up: null | WfcSampleCell;
+	down: null | WfcSampleCell;
+	left: null | WfcSampleCell;
+	right: null | WfcSampleCell;
 }
