df8e3450b267c50b5c67bad332350a39d664c413
Marcin Jerzak <hey@jerzakm.com>
2022-09-02 11:31:42 +0200

    three renderer

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,3 @@
-<script lang="ts">
-	import Counter from '$lib/Counter.svelte';
-</script>
-
 <svelte:head>
 	<title>Hexmap</title>
 	<meta name="description" content="Svelte demo app" />
@@ -22,21 +18,5 @@
 
 	h1 {
 		width: 100%;
-	}
-
-	.welcome {
-		display: block;
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
-
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
 	}
 </style>
---
+++ src/routes/hexmap/+page.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+	import { initHexmap } from './_hexmap';
+	let canvas: HTMLCanvasElement | undefined;
+
+	onMount(() => {
+		if (canvas) {
+			initHexmap(canvas);
+		}
+	});
+</script>
+
+<svelte:head>
+	<title>About</title>
+	<meta name="description" content="About this app" />
+</svelte:head>
+
+<canvas bind:this={canvas} />
+
+<style>
+	canvas {
+		width: 100vw;
+		height: 100vh;
+		position: fixed;
+		top: 0;
+		left: 0;
+	}
+</style>
