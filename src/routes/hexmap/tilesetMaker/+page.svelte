bcbbc5f9635de72a3a63f266e22a9670b2973e34
Marcin Jerzak <hey@jerzakm.com>
2022-09-03 21:31:00 +0200

    tileset color picking

--- src/routes/hexmap/tilesetMaker/+page.svelte
+++ src/routes/hexmap/tilesetMaker/+page.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
 
+	import * as PIXI from 'pixi.js';
+
 	import * as Honeycomb from 'honeycomb-grid';
 
 	import type { WfcHexTile, WfcTileSet } from '$lib/hexmap/tileset';
@@ -14,8 +16,15 @@
 	let rawTiles: any;
 
 	let canvas: HTMLCanvasElement;
+	let previewColor = [0, 0, 0];
 
 	let pickTile: any;
+
+	let newTag = {
+		name: '',
+		color: `0x000000`
+	};
+	let pickingColor = false;
 
 	onMount(async () => {
 		const res = await fetch('/api/tileset');
@@ -23,24 +32,21 @@
 		tileSet = data;
 
 		const ctx = canvas.getContext('2d');
+		if (ctx) ctx.imageSmoothingEnabled = false;
 
-		const hexSize = 48;
-		const padding = 6;
+		const hexSize = 96;
+		const padding = -4;
 
 		const Hex = Honeycomb.extendHex({ size: hexSize + padding, orientation: 'flat' });
 		const Grid = Honeycomb.defineGrid(Hex);
 
 		const centerHex = Hex(1, 1);
 
-		drawHexgrid();
+		let imgData: ImageData | undefined;
 
 		pickTile = async (tile: WfcHexTile) => {
-			const { x, y } = centerHex.toPoint();
-			const image = await loadImage(tile.path);
-			ctx?.drawImage(image, x + padding, y - 12 + padding);
-		};
-
-		function drawHexgrid() {
+			ctx?.clearRect(0, 0, canvas.width, canvas.height);
+			// draw grid
 			Grid.hexagon({ radius: 1, center: Hex(1, 1) }).forEach((hex) => {
 				const point = hex.toPoint();
 				// add the hex's position to each of its corner points
@@ -63,18 +69,64 @@
 				ctx.lineTo(firstCorner.x, firstCorner.y);
 				ctx?.stroke();
 			});
-		}
 
+			// draw tile on top of the grid
+
+			const { x, y } = centerHex.toPoint();
+			const image = await loadImage(tile.path);
+			ctx?.drawImage(image, x + padding, y - 24 + padding, image.width * 2, image.height * 2);
+
+			imgData = ctx?.getImageData(0, 0, canvas.width, canvas.height);
+		};
 		pickTile(tileSet.tiles[0]);
-	});
 
-	function drawTile(tile: WfcHexTile) {
-		//
-	}
+		canvas.addEventListener('mousemove', (e) => {
+			const x = e.offsetX;
+			const y = e.offsetY;
+
+			if (!imgData) return;
+			let index = (y * imgData.width + x) * 4;
+
+			let red = imgData.data[index];
+			let green = imgData.data[index + 1];
+			let blue = imgData.data[index + 2];
+			let alpha = imgData.data[index + 3];
+
+			previewColor = [red, green, blue];
+		});
+	});
 </script>
 
 <h1>{tileSet.name}</h1>
-<tags> ... </tags>
+<tags class="flex">
+	<div class="flex  bg-slate-200 p-4 rounded-lg gap-2 ">
+		<div class="flex flex-col gap-1">
+			<span class="font-bold">Add a new tag</span>
+			<div class="flex gap-2">
+				<span>name</span>
+				<input class="w-3/4" bind:value={newTag.name} />
+			</div>
+
+			<div class="flex gap-2 items-center">
+				<span>color</span>
+				<input class="w-32" bind:value={newTag.color} />
+			</div>
+		</div>
+		<div class="mt-6">
+			<button class="bg-white w-full" on:click={() => (pickingColor = !pickingColor)}>k</button>
+			<div
+				class="w-8 h-8 "
+				style={`background-color: rgb(${previewColor[0]},${previewColor[1]},${previewColor[2]});`}
+			/>
+		</div>
+		<button class="mt-4 bg-white font-bold border-slate-800 border w-16">Add</button>
+	</div>
+	<li>
+		{#each tileSet.tags as tag}
+			<ul>{tag.name}</ul>
+		{/each}
+	</li>
+</tags>
 <editor>
 	<tiles class="tiles-todo">
 		{#each tileSet.tiles as tile}
@@ -85,7 +137,12 @@
 			{/if}
 		{/each}
 	</tiles>
-	<canvas bind:this={canvas} width="500" height="500" />
+	<div class="canvas-container relative">
+		{#if pickingColor}<span class="absolute top-0 text-red-600 font-bold"
+				>picking new color for tile tag</span
+			>{/if}
+		<canvas bind:this={canvas} width="800" height="800" />
+	</div>
 	<tiles class="tiles-done">
 		{#each tileSet.tiles as tile}
 			{#if tile.sideTags}
@@ -104,5 +161,12 @@
 	editor {
 		display: flex;
 		align-items: flex-start;
+		gap: 1rem;
+	}
+
+	.canvas-container {
+		display: flex;
+		flex-direction: column;
+		align-items: start;
 	}
 </style>
