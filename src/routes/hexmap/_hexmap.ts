7fd73df4018926ae21e30939e1a482d486044cdf
Marcin Jerzak <hey@jerzakm.com>
2022-09-04 14:11:10 +0200

    hexmap generation mvp

--- src/routes/hexmap/_hexmap.ts
+++ src/routes/hexmap/_hexmap.ts
@@ -10,7 +10,12 @@ export const initHexmap = () => {
 	const map = new PIXI.Container();
 	app.stage.addChild(map);
 
+	app.stage.scale.x = 0.7;
+	app.stage.scale.y = 0.7;
+
 	cameraSetup(app, map);
+
+	const gridSize: [number, number] = [30, 10];
 
 	const hex = 'hexTileSets/ZeshiosPixelHexTileset1.1/PixelHex_zeshio_tile-001.png';
 	const texture = PIXI.Texture.from(hex);
@@ -22,7 +27,7 @@ export const initHexmap = () => {
 	const Grid = Honeycomb.defineGrid(Hex);
 
 	// draw hex grid - border outlines
-	const hexGrid = Grid.rectangle({ width: 10, height: 10 });
+	const hexGrid = Grid.rectangle({ width: gridSize[0], height: gridSize[1] });
 
 	hexGrid.map((hex) => {
 		const point = hex.toPoint();
@@ -36,7 +41,7 @@ export const initHexmap = () => {
 	});
 
 	// WFC starts here
-	wfc(hexGrid, map);
+	wfc(gridSize, map);
 
 	// center the map
 	map.x = app.renderer.width / 2 - map.width / 2;
@@ -69,30 +74,131 @@ const cameraSetup = (app: PIXI.Application, map: PIXI.Container) => {
 	});
 };
 
-const wfc = async (
-	hexGrid: Honeycomb.Grid<
-		Honeycomb.Hex<{
-			size: number;
-			orientation: 'flat';
-		}>
-	>,
-	map: PIXI.Container
-) => {
-	const wfcHexTiles: WfcHexTile[] = [];
-
+const wfc = async (gridSize: [number, number], map: PIXI.Container) => {
 	// 1. Load tileset
+	const { tiles, tags } = await fetchTileset();
 
-	const tileSet = await fetchTileset();
-	console.log(tileSet);
+	// 2. Make grid and setup data structure for wfc cells
+	const Hex = Honeycomb.extendHex({ size: 48, orientation: 'flat' });
+	const Grid = Honeycomb.defineGrid(Hex);
+
+	// draw hex grid - border outlines
+	const hexGrid = Grid.rectangle({ width: gridSize[0], height: gridSize[1] });
+
+	const wfcHexTiles: WfcHexTile[] = [];
+	const wfcHexGrid: WfcHexTile[][] = Array(gridSize[0])
+		.fill(0)
+		.map(() => Array(gridSize[1]));
+
+	// check if every tile edge is labeled
+	const validTiles = [];
+	for (const tile of tiles) {
+		if (
+			tile.sideTags &&
+			tile.sideTags.every((t) => {
+				return typeof t != 'undefined';
+			})
+		) {
+			//
+			validTiles.push(tile);
+		}
+	}
 
 	for (const hex of hexGrid) {
-		wfcHexTiles.push({
+		const cell = {
 			hex,
 			collapsed: false,
 			tile: undefined,
-			options: []
-		});
+			options: [...validTiles]
+		};
+		wfcHexTiles.push(cell);
+		wfcHexGrid[hex.x][hex.y] = cell;
 	}
+
+	// 3. Pick a random grid cell and collapse it first
+	collapseTile(Math.floor(Math.random() * gridSize[0]), Math.floor(Math.random() * gridSize[1]));
+	collapseStep();
+	map.interactive = true;
+	map.on('click', () => {
+		collapseStep();
+	});
+
+	function collapseTile(x: number, y: number) {
+		const cell = wfcHexGrid[x][y];
+		cell.collapsed = true;
+		cell.tile = cell.options[Math.floor(Math.random() * cell.options.length)];
+		cell.options = [cell.tile];
+
+		const sprite = PIXI.Sprite.from(cell.tile.path);
+		const point = wfcHexGrid[x][y].hex.toPoint();
+		sprite.x = point.x;
+		sprite.y = point.y - 12;
+		map.addChild(sprite);
+
+		// 1.1 After collapsing a tile, recalculate neighbour options
+		const neighbours = hexGrid.neighborsOf(cell.hex);
+		for (let n = 0; n < neighbours.length; n++) {
+			const nHex = neighbours[n];
+			if (!nHex) return;
+
+			const nCell = wfcHexGrid[nHex.x][nHex.y];
+			// if neighbour exists and isnt collapsed, recalculate options
+			if (!nCell.collapsed) {
+				calculateOptions(nCell);
+			}
+		}
+	}
+
+	function calculateOptions(cell: WfcHexTile) {
+		const neighbours = hexGrid.neighborsOf(cell.hex);
+		for (let i = 0; i < 6; i++) {
+			// check each neighbour
+			if (neighbours[i]) {
+				const neighbour = wfcHexGrid[neighbours[i].x][neighbours[i].y];
+				// sideTags of options for an edge where it borders
+				const sideTags: string[] = [];
+
+				neighbour.options.map((option) => {
+					if (option.sideTags) {
+						const tag = option.sideTags[(i + 3) % 6];
+						typeof tag === 'string' ? sideTags.push(tag) : '';
+					}
+				});
+
+				cell.options = cell.options.filter((o) => {
+					if (o.sideTags && o.sideTags[i]) {
+						return sideTags.includes(`${o.sideTags[i]}`);
+					}
+				});
+			}
+		}
+	}
+
+	function collapseStep() {
+		wfcHexTiles.sort((a, b) => {
+			return a.options.length - b.options.length;
+		});
+
+		for (let i = 0; i < wfcHexTiles.length; i++) {
+			if (!wfcHexTiles[i].collapsed) {
+				collapseTile(wfcHexTiles[i].hex.x, wfcHexTiles[i].hex.y);
+				break;
+			}
+		}
+
+		const uncollapsed = wfcHexTiles.find((c) => {
+			return !c.collapsed;
+		});
+
+		if (uncollapsed) {
+			setTimeout(collapseStep, 3);
+			console.count('collapsing');
+		} else {
+			console.log('no more uncollapsed tiles');
+		}
+	}
+
+	console.log(wfcHexTiles);
 };
 
 interface WfcHexTile {
